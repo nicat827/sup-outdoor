25a17f286d2847e51c843429d87f460ec88d9c95
nicat827 <nici.mecidov@list.ru>
2024-03-23 15:55:30 +0400

    bugs fixed

--- scripts/services.js
+++ scripts/services.js
@@ -1,21 +1,28 @@
-const upBtn = document.querySelector("#goToUpBtn")
-window.addEventListener("scroll", () => {
-    if (this.window.scrollY > 0) {
-        upBtn.style.display = 'block';
-    } else {
-        upBtn.style.display = 'none';
+document.addEventListener("DOMContentLoaded", () => {
+    if (window.innerWidth <= 400) {
+        let hiddenSpans = document.querySelectorAll(".empty-text")
+        hiddenSpans.forEach(span => span.innerHTML = "this is")
     }
-})
-
-upBtn.addEventListener("click", () => {
-    const scrollDuration = 500; // Продолжительность анимации в миллисекундах
-    const scrollStep = -window.scrollY / (scrollDuration / 15); // Шаг скролла
-
-    const scrollInterval = setInterval(function() {
-        if (window.scrollY !== 0) {
-            window.scrollBy(0, scrollStep);
+    const upBtn = document.querySelector("#goToUpBtn")
+    window.addEventListener("scroll", () => {
+        if (this.window.scrollY > 0) {
+            upBtn.style.display = 'block';
         } else {
-            clearInterval(scrollInterval);
+            upBtn.style.display = 'none';
         }
-    }, 15);
+    })
+    
+    upBtn.addEventListener("click", () => {
+        const scrollDuration = 500; // Продолжительность анимации в миллисекундах
+        const scrollStep = -window.scrollY / (scrollDuration / 15); // Шаг скролла
+    
+        const scrollInterval = setInterval(function() {
+            if (window.scrollY !== 0) {
+                window.scrollBy(0, scrollStep);
+            } else {
+                clearInterval(scrollInterval);
+            }
+        }, 15);
+    })
+
 })
